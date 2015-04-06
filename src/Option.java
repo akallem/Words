d10916ed816dd03c2f88b68e2cd2b26946ba99a8
ZhangWangda <zhangwangda@gmail.com>
2015-04-06 17:08:45 -0400

    Update Option.java

--- src/Option.java
+++ src/Option.java
@@ -1,5 +1,5 @@
 public class Option {
 	public static boolean GUI = true;
-    public static int TIME_TO_WAIT = 1000;
-    public static int MAX_COUNTER = 20;
+	public static int TIME_TO_WAIT = 1000;
+	public static int MAX_COUNTER = 20;
 }
