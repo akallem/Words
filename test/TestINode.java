7f109a1ef26e4bf5e26c12f1cb0066b78ada8638
Alex Mark <acm2209@columbia.edu>
2015-04-05 20:17:50 -0400

    Add test for iteration

--- test/TestINode.java
+++ test/TestINode.java
@@ -2,13 +2,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class TestINode {
 	// Some generic variables useful for all tests
 	WordsEnvironment environment = new WordsEnvironment();
+	FrameLoop loop = new FrameLoop(environment);
 
 	AST nothingLeaf = new LNode(AST.ASTType.NOTHING, 0);
 	AST numLeaf = new LNode(AST.ASTType.NUM, 0, 0.0);
+	AST twoLeaf = new LNode(AST.ASTType.NUM, 0, 2.0);
+	AST fiveLeaf = new LNode(AST.ASTType.NUM, 0, 5.0);
 	AST stringLeaf = new LNode(AST.ASTType.STRING, 0, "string");
+	AST fredStringLeaf = new LNode(AST.ASTType.STRING, 0, "Fred");
+	AST leftDirectionLeaf = new LNode(AST.ASTType.DIRECTION, 0, Direction.Type.LEFT);
+	AST rightDirectionLeaf = new LNode(AST.ASTType.DIRECTION, 0, Direction.Type.RIGHT);
+	
+	AST moveLeft2 = new INode(AST.ASTType.QUEUE_MOVE, 0, nothingLeaf, fredStringLeaf, leftDirectionLeaf, twoLeaf, null);
+	AST moveRight2 = new INode(AST.ASTType.QUEUE_MOVE, 0, nothingLeaf, fredStringLeaf, rightDirectionLeaf, twoLeaf, null);
 		
 	/*********************
 	 * evalPosition
@@ -43,4 +53,46 @@ public class TestINode {
 		INode testNode = new INode(AST.ASTType.POSITION, 0, numLeaf, stringLeaf);
 		testNode.eval(environment);
 	}
+	
+	/*********************
+	 * evalQueueMove
+	 ********************/
+	@Test
+	public void testWorkingMove() throws WordsRuntimeException {
+		environment.createObject("Fred", "thing", new WordsPosition(0,0));
+		loop.fastForwardEnvironment(1);
+		assertEquals("Fred in good start position", environment.getObject("Fred").getCurrentCell(), new WordsPosition(0,0));
+		loop.enqueueAST(moveLeft2);
+		loop.fastForwardEnvironment(1);
+		assertEquals("Fred moved", environment.getObject("Fred").getCurrentCell(), new WordsPosition(-1,0));
+		loop.fastForwardEnvironment(1);
+		assertEquals("Fred moved", environment.getObject("Fred").getCurrentCell(), new WordsPosition(-2,0));
+	}
+	
+	/*********************
+	 * evalIteration
+	 ********************/
+	@Test
+	public void testWorkingIteration() throws WordsRuntimeException {
+		AST statementList = new INode(AST.ASTType.STATEMENT_LIST, 0, moveLeft2, moveRight2);
+		AST iterativeLoop = new INode(AST.ASTType.REPEAT, 0, fiveLeaf, statementList);
+		environment.createObject("Fred", "thing", new WordsPosition(0,0));
+		loop.enqueueAST(iterativeLoop);
+		loop.fastForwardEnvironment(1);
+		assertEquals("Fred in good start position", environment.getObject("Fred").getCurrentCell(), new WordsPosition(0,0));
+		loop.fastForwardEnvironment(2);
+		assertEquals("Fred makes first move", environment.getObject("Fred").getCurrentCell(), new WordsPosition(-2,0));
+		loop.fastForwardEnvironment(2);
+		assertEquals("Fred returns from move", environment.getObject("Fred").getCurrentCell(), new WordsPosition(0,0));
+		loop.fastForwardEnvironment(20);
+		assertEquals("Fred ends in correct place", environment.getObject("Fred").getCurrentCell(), new WordsPosition(0,0));
+	}
+	
+	@Test (expected = WordsInvalidTypeException.class)
+	public void testBadIteration() throws WordsRuntimeException {
+		AST statementList = new INode(AST.ASTType.STATEMENT_LIST, 0, moveLeft2, moveRight2);
+		AST iterativeLoop = new INode(AST.ASTType.REPEAT, 0, stringLeaf, statementList);
+		iterativeLoop.eval(environment);
+	}
+	
 }
