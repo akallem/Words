063a6de82284c5fabf3b6ef25862571972d1eafb
Andrew Kallem <ajk2214@columbia.edu>
2015-04-07 07:36:18 -0400

    Fix formatString error in WordsObjectNotFoundException which was causing a string format exception

--- src/WordsObjectNotFoundException.java
+++ src/WordsObjectNotFoundException.java
@@ -13,6 +13,6 @@ public class WordsObjectNotFoundException extends WordsRuntimeException {
 	
 	@Override
 	public String toString() {
-		return String.format("Error: At line %d, object %s did not exist.", objectName);
+		return String.format("object %s did not exist.", objectName);
 	}
 }
