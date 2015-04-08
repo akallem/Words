8a1a3e30c87bca06b3fcab33c3887afa89280125
Michael Ben-Ami <mike.benami@gmail.com>
2015-04-08 01:54:25 -0400

    Test for INodeAssign

--- test/TestINodeAssign.java
+++ test/TestINodeAssign.java
@@ -3,22 +3,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestINodeAssign extends TestINode {
-	reference reference_list identifier IS value_expression '.'					{ $$ = new INodeAssign((new INodeReferenceList($1)).add(((INode) $2).children), $3, $5); ((AST) $$).lineNumber = lexer.lineNumber; }
 	
 	@Test
 	public void assignProperty() throws WordsRuntimeException {
 		WordsObject alexObject = environment.createObject("Alex", "thing", new WordsPosition(0,0));
-		WordsObject bobObject = environment.createObject("Bob", "thing", new WordsPosition(0,0));
-		
 		LNodeReference alexLNodeRef = new LNodeReference("Alex's");
 		INodeReferenceList alexRefList = new INodeReferenceList(alexLNodeRef);
 		
+		WordsObject bobObject = environment.createObject("Bob", "thing", new WordsPosition(0,0));
 		LNodeIdentifier bobIdentifier = new LNodeIdentifier("Bob");
 		
 		INodeAssign numAssign = new INodeAssign(alexRefList, new LNodeIdentifier("height"), fiveLeaf);
 		INodeAssign stringAssign = new INodeAssign(alexRefList, new LNodeIdentifier("greeting"), stringLeaf);
 		INodeAssign objAssign = new INodeAssign(alexRefList, new LNodeIdentifier("friend"), new INodeReferenceList(), bobIdentifier);
 		INodeAssign nothingAssign = new INodeAssign(alexRefList, new LNodeIdentifier("myNothing"), new LNodeNothing());
-
+		
+		assertEquals("Number assignment successful", alexObject.getProperty("height").numProperty, 5.0);
+		assertEquals("String assignment successful", alexObject.getProperty("greeting").stringProperty, "string");
+		assertEquals("Object assignment successful", alexObject.getProperty("friend"), bobObject);
+		assertEquals("Nothing assignment successful", alexObject.getProperty("myNothing", WordsProperty.PropertyType.NOTHING);
 	}
 }
