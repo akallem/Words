e163c839f034c4dd9f23688ef179f711465efe7f
Alex Mark <acm2209@columbia.edu>
2015-04-09 16:30:41 -0400

    Only print file name, not full file path during system test

--- system_test/test.sh
+++ system_test/test.sh
@@ -4,10 +4,10 @@ for prog in "$@"; do
     java -jar jar/Words.jar "$prog" -testmode > run.log.tmp
     
     if ! diff run.log.tmp "$prog.log"; then
-    	echo "[System Test] $prog FAILED  Check run.log.tmp.  Aborting..."
+    	echo "[System Test] ${prof##*/} FAILED  Check run.log.tmp.  Aborting..."
     	exit 1
     fi
-    echo "[System Test] $prog OK"
+    echo "[System Test] ${prog##*/} OK"
 done
 
 rm run.log.tmp
