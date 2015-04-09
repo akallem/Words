651f7a45fb5e38131bd6f3eba337ff35b21798f1
jtyoung1292 <jtyoung1292@gmail.com>
2015-04-09 15:19:41 -0400

    added tests

---
+++ test/words/test/TestINodeExponentiate.java
@@ -0,0 +1,26 @@
+package words.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import words.ast.*;
+import words.exceptions.*;
+
+public class TestINodeExponentiate extends TestINode {
+	@Test
+	public void twoNumbersExponentiateCorrectly() throws WordsRuntimeException {
+		AST numLeaf1 = new LNodeNum(4);
+		AST numLeaf2 = new LNodeNum(3);
+		
+		INode testNode = new INodeExponentiate(numLeaf1, numLeaf2);
+		ASTValue result = testNode.eval(environment);
+		assertTrue("Returns NUM value", result.type == ASTValue.ValueType.NUM);
+		assertEquals("Correct Exponentiation", result.numValue, 64.0, 0.0001);
+	}
+	
+	@Test (expected = WordsArithmeticException.class)
+	public void onlyOperatesOnTwoNumbers() throws WordsRuntimeException {
+		INode testNode = new INodeExponentiate(numLeaf, stringLeaf);
+		ASTValue result = testNode.eval(environment);
+	}
+}
