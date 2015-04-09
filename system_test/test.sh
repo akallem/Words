d3b16a6b65e36cf33fca8d14b2eaffc5192d23f2
Alex Mark <acm2209@columbia.edu>
2015-04-09 18:40:57 -0400

    Fix typo

--- system_test/test.sh
+++ system_test/test.sh
@@ -4,7 +4,7 @@ for prog in "$@"; do
     java -jar jar/Words.jar "$prog" -testmode > run.log.tmp
     
     if ! diff run.log.tmp "$prog.log"; then
-    	echo "[System Test] ${prof##*/} FAILED  Check run.log.tmp.  Aborting..."
+    	echo "[System Test] ${prog##*/} FAILED  Check run.log.tmp.  Aborting..."
     	exit 1
     fi
     echo "[System Test] ${prog##*/} OK"
