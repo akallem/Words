17f47db7bdd3aa73e0fe59261a1378c38475dbbe
Andrew Kallem <ajk2214@columbia.edu>
2015-04-09 18:26:09 -0400

    Modified Windows batch script for system test to print only filename and not full path

--- system_test/test.bat
+++ system_test/test.bat
@@ -4,7 +4,7 @@
 if "%~1" neq "" (
   java -jar jar/Words.jar %1 -testmode > run.log.tmp
 
-  fc run.log.tmp %1.log >nul 2>&1 && echo [System Test] %1 OK || (echo [System Test] %1 FAILED  Check run.log.tmp.  Aborting...  & exit 1)
+  fc run.log.tmp %1.log >nul 2>&1 && echo [System Test] %~n1%~x1 OK || (echo [System Test] %~n1%~x1 FAILED  Check run.log.tmp.  Aborting...  & exit 1)
   
   shift
   goto :loop
