647fcf75b15fadf7a789ca7f77a52c86a7a05412
Andrew Kallem <ajk2214@columbia.edu>
2015-04-20 10:22:38 -0400

    Update Direction in TestINodeQueueStop

--- test/words/test/TestINodeQueueStop.java
+++ test/words/test/TestINodeQueueStop.java
@@ -14,8 +14,12 @@ public class TestINodeQueueStop extends TestINode {
 		environment.createObject("Fred", "thing", new Position(0,0));
 		loop.fastForwardEnvironment(1); //object is created with a 1 frame wait, so use it up. 
 		assertEquals("Fred in good start position", environment.getObject("Fred").getCurrentPosition(), new Position(0,0));
-		environment.getObject("Fred").enqueueAction(new MoveAction(new Direction(Direction.Type.RIGHT), new LNodeNum(1)));
-		environment.getObject("Fred").clearActionQueue();
+		environment.getObject("Fred").enqueueAction(new MoveAction(Direction.RIGHT, new LNodeNum(1)));
+		
+		
+		AST idLeaf = new LNodeIdentifier("Fred");
+		INode testNode = new INodeQueueStop(new INodeReferenceList(), idLeaf);
+		testNode.eval(environment);
 		loop.fastForwardEnvironment(1);
 		assertEquals("Fred never moved", environment.getObject("Fred").getCurrentPosition(), new Position(0,0));
 	}
