aa2133245e8deaba61a00484cf7e072c69a66d35
Michael Ben-Ami <mike.benami@gmail.com>
2015-04-21 00:47:13 -0400

    looks good, need unit test

--- src/words/ast/INodeRemoveObject.java
+++ src/words/ast/INodeRemoveObject.java
@@ -1,26 +1,30 @@
 package words.ast;
 
 import words.environment.*;
+import words.environment.Property.PropertyType;
 import words.exceptions.*;
 
 public class INodeRemoveObject extends INode {
 	public INodeRemoveObject(Object... children) {
 		super(children);
-	}
+	}	
 
 	@Override
 	public ASTValue eval(Environment environment) throws WordsRuntimeException {
 		WordsObject obj = null;
 		ASTValue refList = children.get(0).eval(environment);
+		ASTValue id = children.get(1).eval(environment);
 		
 		if (refList.type == ASTValue.Type.NOTHING) {
-			ASTValue id = children.get(1).eval(environment);
 			obj = environment.getObject(id.stringValue);
 		} else {
-			obj = refList.objValue;
+			Property property = refList.objValue.getProperty(id.stringValue);
+			obj = property.objProperty;
+			if (obj == null) {
+				throw new ReferenceException(id.stringValue, property.type);
+			}
 		}
 		
-		assert obj != null : "Obj was null when it shouldn't have been.";
 		environment.removeObject(obj);
 		
 		return null;
