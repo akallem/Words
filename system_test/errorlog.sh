9b263275807f8a3530777c2b2c1035d99962512d
Wangda <zhangwangda@gmail.com>
2015-04-24 00:55:55 -0400

    integrated buildlog

--- system_test/errorlog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for prog in "$@"; do
-    echo "Logging $prog ..."
-    java -enableassertions -jar jar/Words.jar "$prog" -testmode -displayerror 2>&1 | grep -i "error" >"$prog.log"
-done
