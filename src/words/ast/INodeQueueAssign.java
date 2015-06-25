7ce3453cb36f3260927d1d4500dac416fe497737
Alex Mark <acm2209@columbia.edu>
2015-06-25 15:12:22 -0400

    Fix queued assignment bug

--- src/words/ast/INodeQueueAssign.java
+++ src/words/ast/INodeQueueAssign.java
@@ -12,6 +12,10 @@ public class INodeQueueAssign extends INode {
 	@Override
 	public Variable eval(Environment environment) throws WordsRuntimeException {
 		Variable referenceObject = children.get(0).eval(environment);
+		// Right now, we don't support queued assignments on local variables
+		if (referenceObject.type != Variable.Type.OBJ) {
+			throw new InvalidTypeException("Object", referenceObject.type.toString());
+		}
 		AST propertyList = children.get(1);
 		Variable doNow = children.get(2) != null ? children.get(2).eval(environment) : null;
 		
